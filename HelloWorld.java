089e30806f3c3bfe2f102b3d43a6d3126a876079
monywaghost <monywaghost@gmail.com>
2017-07-17 13:13:14 +0630

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld
       public static void main(String[] args)
       {       
          System.out.println("Hello jenkins with github, I'm musi@DAT!");
-         System.out.println("Are you ready?");
+         System.out.println("Are you ready?, jenkins used for CI");
          
       }
 } 
