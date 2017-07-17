575606381f81a5567df4904a4d3a8b4253b44646
monywaghost <monywaghost@gmail.com>
2017-07-17 13:30:28 +0630

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,6 @@ public class HelloWorld
       public static void main(String[] args)
       {       
          System.out.println("Hello jenkins with github, I'm musi@DAT!");
-         System.out.println("What is continuous integration?");
-         
+                  
       }
 } 
